a2a05762444c2463cd5dbe2c5685943a504aaaf4
taiyu <taiyu.len@gmail.com>
2020-05-29 00:45:17 -0700

    twitter: fix url to provide info we need.
    previously it would only work with tweets that have 1 image, and would
    not include the media object.

--- source/chrome/content/sites/anktwitter.js
+++ source/chrome/content/sites/anktwitter.js
@@ -218,7 +218,35 @@ Components.utils.import("resource://gre/modules/Task.jsm");
         };
 
         // make request
-        const url = `https://api.twitter.com/2/timeline/conversation/${illust_id}.json`;
+        // The options used by the browser client are the following.
+        // the one we need to get access to the media entries is.
+        // `tweet_mode=extended`
+        //
+        // include_profile_interstitial_type=1
+        // include_blocking=1
+        // include_blocked_by=1
+        // include_followed_by=1
+        // include_want_retweets=1
+        // include_mute_edge=1
+        // include_can_dm=1
+        // include_can_media_tag=1
+        // skip_status=1
+        // cards_platform=Web-12
+        // include_cards=1
+        // include_composer_source=true
+        // include_ext_alt_text=true
+        // include_reply_count=1
+        // tweet_mode=extended
+        // include_entities=true
+        // include_user_entities=true
+        // include_ext_media_color=true
+        // include_ext_media_availability=true
+        // send_error_codes=true
+        // simple_quoted_tweet=true
+        // count=20
+        // ext=mediaStats%2ChighlightedLabel%2CcameraMoment
+        // include_quote_count=true
+        const url = `https://api.twitter.com/2/timeline/conversation/${illust_id}.json?tweet_mode=extended`;
         const json = JSON.parse(yield AnkUtils.httpGETAsync(url, referer, headers));
 
         if (json.errors)
